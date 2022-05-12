f20cdaefc80a5739c2be6f69873d5148aaaffb38
cirejm07 <moreno.jeric1995@gmail.com>
2022-05-13 01:36:45 +0800

    initial commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ $(window).scroll(function(){
   }, 100);
 });
 
-// Hovering the "coding word"
+// Hovering the "coding word" a tool-tip will apprear
     $('.ingredients-logo-container').hover(
     function(){$(this).find(".ingredients-logo-text").fadeIn(150);},
     function(){$(this).find(".ingredients-logo-text").fadeOut(300);},
@@ -36,6 +36,7 @@ $(window).scroll(function(){
            )},
        function(){$('#interested-in-coding a').css("visibility","hidden")}
        )
+
 // Hovering the tool-tip anchor tag
     $('#interested-in-coding a').hover(
         function(){$(this).css(
@@ -45,4 +46,84 @@ $(window).scroll(function(){
             )},
         function(){$(this).css("visibility","hidden")}
         )
+
+    // when hover, the parent will find it child element "cards-title" and add a class title of "hide-element"
+    $(".cards").hover(
+      function(){$(this).find(".cards-title").addClass("hide-element")},
+      function(){$(this).find(".cards-title").removeClass("hide-element")},
+      )
+
+    // Show project description and anchor tag
+    $(".cards").hover(
+    function(){$(this).find(".view-project").slideToggle(500);}
+    )
+
+    // slider testimonial
+    card=1;
+    $(".prev").click(function(){
+       if(card==0)
+        {
+            $("#side1").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side3").css("z-index","9");
+            $("#side1").css("transform","translateX(0px) scale(1.5)");
+            $("#side2").css("transform","translateX(-100px) scale(1)");
+            $("#side3").css("transform","translateX(100px)");
+            card=1;
+        }
+        else if(card==1)
+        {
+            $("#side3").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side3").css("transform","translateX(0px) scale(1.5)");
+            $("#side1").css("transform","translateX(-100px) scale(1)");
+            $("#side2").css("transform","translateX(100px)");
+            card=2;
+        }
+        else if(card==2)
+        {
+            $("#side2").css("z-index","999");
+            $("#side3").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side2").css("transform","translateX(0px) scale(1.5)");
+            $("#side3").css("transform","translateX(-100px) scale(1)");
+            $("#side1").css("transform","translateX(100px)");
+            card=0;
+        }
+    });
+    $(".next").click(function(){
+       if(card==0)
+        {
+            $("#side3").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side3").css("transform","translateX(0px) scale(1.5)");
+            $("#side1").css("transform","translateX(-100px) scale(1)");
+            $("#side2").css("transform","translateX(100px)");
+            card=2;
+        }
+        else if(card==1)
+        {
+            $("#side2").css("z-index","999");
+            $("#side3").css("z-index","9");
+            $("#side1").css("z-index","9");
+            $("#side2").css("transform","translateX(0px) scale(1.5)");
+            $("#side3").css("transform","translateX(-100px) scale(1)");
+            $("#side1").css("transform","translateX(100px)");
+            card=0;
+        }
+        else if(card==2)
+        {
+            $("#side1").css("z-index","999");
+            $("#side2").css("z-index","9");
+            $("#side3").css("z-index","9");
+            $("#side1").css("transform","translateX(0px) scale(1.5)");
+            $("#side2").css("transform","translateX(-100px) scale(1)");
+            $("#side3").css("transform","translateX(100px)");
+            card=1;
+        }
+    });    
+
+
 });
